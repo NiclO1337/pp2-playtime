f9212c3057e3ed49562acaf55cb26d54530d58f5
NiclO1337 <niclas.hugdahl@gmail.com>
2023-09-28 17:50:08 +0200

    Add somewhat working function after many error

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,22 @@ console.log("Hello");
 
 function changeColorTheme() {
   let buttons = document.getElementsByClassName("btn");
-  console.log(buttons);
-}
+   for (let button of buttons) {
+    if (button.classList.contains('btn-blue')) {
+      console.log("yes");
+    } else {
+      console.log("no")
+    }
+  }
+   }
+
+  // buttons.addEventListener('click', function changeColor() {
+  //   if (buttons hasClass(btn-blue))
+  // })
+
+
+
+
 
 changeColorTheme();
+
