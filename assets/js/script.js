32956926efbe140f2bce4a0295c5c9b40c74d2b5
NiclO1337 <niclas.hugdahl@gmail.com>
2023-09-28 18:06:59 +0200

    Add more to somewhat working function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
-console.log("Hello");
-
+/**
+ * Function to change color theme based on which button is pressed
+ */
 function changeColorTheme() {
   let buttons = document.getElementsByClassName("btn");
-   for (let button of buttons) {
+
+  for (let button of buttons) {
     if (button.classList.contains('btn-blue')) {
-      console.log("yes");
-    } else {
-      console.log("no")
+      console.log("blue");
+      document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)')
+    } else if (button.classList.contains('btn-red')) {
+      console.log("red");
+    } else if (button.classList.contains('btn-green')) {
+      console.log("green");
+    } else if (button.classList.contains('btn-random')) {
+      console.log("random");
+    } else if (button.classList.contains("color-input")) {
+      console.log("input-color");
     }
   }
-   }
+}
 
-  // buttons.addEventListener('click', function changeColor() {
-  //   if (buttons hasClass(btn-blue))
-  // })
+
+// buttons.addEventListener('click', function changeColor() {
+//   if (buttons hasClass(btn-blue))
+// })
 
 
 
