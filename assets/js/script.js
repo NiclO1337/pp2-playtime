88555746dca919349b7406e6b38bca346cc34eb9
NiclO1337 <niclas.hugdahl@gmail.com>
2023-09-28 22:51:32 +0200

    Add non working random color generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Function to change color theme based on which button is pressed
+ */
 document.addEventListener('DOMContentLoaded', function() {
   let buttons = document.getElementsByClassName("btn");
 
@@ -8,14 +11,22 @@ document.addEventListener('DOMContentLoaded', function() {
       } else if (button.classList.contains('btn-red')) {
         document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
       }  else if (button.classList.contains('btn-green')) {
-        document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)')
+        document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
       } else if (button.classList.contains('btn-random')) {
+        let hexValues = '0123456789ABCDEF';
+        let hashtag = '#';
+        for (value of hexValues) {
+          hashtag += hexValues[Math.floor(Math.random() * 16)];
+        }
+        document.documentElement.style.setProperty('--color-primary', 'hashtag');
         console.log("random");
       } else if (button.classList.contains('color-input')) {
         let input = document.getElementById('color-input');        
         input.addEventListener('input', function inputColor() {
           console.log(input.value);
-          document.documentElement.style.setProperty('--color-primary', 'input.value')
+          let newValue = input.value;
+          console.log(newValue);
+          document.documentElement.style.setProperty('--color-primary', 'newValue')
           }) 
       }
     })
@@ -24,9 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-/**
- * Function to change color theme based on which button is pressed
- */
+
 // function changeColorTheme() {
 //   let buttons = document.getElementsByClassName("btn");
 
