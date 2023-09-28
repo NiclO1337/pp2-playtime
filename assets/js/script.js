6cce7ef3f3672af4a33a4a6053c5a1ad3253fe2a
NiclO1337 <niclas.hugdahl@gmail.com>
2023-09-28 18:35:27 +0200

    Fix broken code, blue button works

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,22 @@
 function changeColorTheme() {
   let buttons = document.getElementsByClassName("btn");
 
+
   for (let button of buttons) {
-    if (button.classList.contains('btn-blue')) {
-      console.log("blue");
-      document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)')
-    } else if (button.classList.contains('btn-red')) {
-      console.log("red");
-    } else if (button.classList.contains('btn-green')) {
-      console.log("green");
-    } else if (button.classList.contains('btn-random')) {
-      console.log("random");
-    } else if (button.classList.contains("color-input")) {
-      console.log("input-color");
-    }
+    button.addEventListener('click', function changeColor() {
+      if (button.classList.contains('btn-blue')) {
+        console.log("blue");
+        document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)')
+      } else if (button.classList.contains('btn-red')) {
+        console.log("red");
+      } else if (button.classList.contains('btn-green')) {
+        console.log("green");
+      } else if (button.classList.contains('btn-random')) {
+        console.log("random");
+      } else if (button.classList.contains("color-input")) {
+        console.log("input-color");
+      }
+    })
   }
 }
 
