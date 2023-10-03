41d74155d4659ee45ace297f60b051658df2ba97
NiclO1337 <niclas.hugdahl@gmail.com>
2023-10-03 14:26:50 +0200

    Changed back, new function did not work

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
   for (let button of buttons) {
     button.addEventListener('click', function () {
-      if (button.classList.contains('theme-btn')) {
-        changeColor();
+      if (button.classList.contains('btn-blue')) {
+        document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)');
+      } else if (button.classList.contains('btn-red')) {
+        document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
+      } else if (button.classList.contains('btn-green')) {
+        document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
+      } else if (button.classList.contains('btn-random')) {
+        let hexValues = '0123456789ABCDEF';
+        let hashtag = '#';
+        for (let values = 0; values < 6; values++) {
+          hashtag += hexValues[Math.floor(Math.random() * 16)];
+          document.documentElement.style.setProperty('--color-primary', hashtag);
+        }
+      } else if (button.classList.contains('color-input')) {
+        let input = document.getElementById('color-input');
+        input.addEventListener('input', function inputColor() {
+          document.documentElement.style.setProperty('--color-primary', input.value);
+        })
       } else if (button.classList.contains('play-game-btn')) {
         let introScreen = document.getElementById('intro-screen');
         introScreen.style.opacity = '0';
@@ -20,68 +36,41 @@ document.addEventListener('DOMContentLoaded', function () {
         window.setTimeout(showGameScreen, 3700)
         window.setTimeout(visibleGameScreen, 3800)
       }
-      // if (button.classList.contains('btn-blue')) {
-      //   document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)');
-      // } else if (button.classList.contains('btn-red')) {
-      //   document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
-      // } else if (button.classList.contains('btn-green')) {
-      //   document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
-      // } else if (button.classList.contains('btn-random')) {
-      //   let hexValues = '0123456789ABCDEF';
-      //   let hashtag = '#';
-      //   for (let values = 0; values < 6; values++) {
-      //     hashtag += hexValues[Math.floor(Math.random() * 16)];
-      //     document.documentElement.style.setProperty('--color-primary', hashtag);
-      //   }        
-      // } else if (button.classList.contains('color-input')) {
-      //   let input = document.getElementById('color-input');
-      //   input.addEventListener('input', function inputColor() {
-      //     document.documentElement.style.setProperty('--color-primary', input.value);
-      //   })
-      // }
-      // if (button.classList.contains('play-game-btn')) {
-      //   let introScreen = document.getElementById('intro-screen');
-      //   introScreen.style.opacity = '0';        
-
-      //   window.setTimeout(hideIntroScreen, 900)
-      //   window.setTimeout(showWelcomeText, 1100)
-      //   window.setTimeout(hideWelcomeText, 2700)
-      //   window.setTimeout(removeWelcomeText, 3600)
-
-      //   window.setTimeout(showGameScreen, 3700)
-      //   window.setTimeout(visibleGameScreen, 3800)
-      // }
     })
   }
 })
 
-function changeColor() {
-  let buttons = document.getElementsByClassName("theme-btn");
+// function changeColor() {
 
-  for (let button of buttons) {
-    button.addEventListener('click', function () {
-      if (button.classList.contains('btn-blue')) {
-          document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)');
-        } else if (button.classList.contains('btn-red')) {
-          document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
-        } else if (button.classList.contains('btn-green')) {
-          document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
-        } else if (button.classList.contains('btn-random')) {
-          let hexValues = '0123456789ABCDEF';
-          let hashtag = '#';
-          for (let values = 0; values < 6; values++) {
-            hashtag += hexValues[Math.floor(Math.random() * 16)];
-            document.documentElement.style.setProperty('--color-primary', hashtag);
-          }        
-        } else if (button.classList.contains('color-input')) {
-          let input = document.getElementById('color-input');
-          input.addEventListener('input', function inputColor() {
-            document.documentElement.style.setProperty('--color-primary', input.value);
-          })
-        }
-    })
-  }
-}
+//   let buttons = document.getElementsByClassName('btn');
+//   for (let button of buttons) {
+
+//     if (button.classList.contains('btn-blue')) {
+//       document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)');
+//       console.log(button);
+//     } else if (button.classList.contains('btn-red')) {
+//       document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
+//       console.log("red");
+//     } else if (button.classList.contains('btn-green')) {
+//       document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
+//       console.log("green");
+//     } else if (button.classList.contains('btn-random')) {
+//       let hexValues = '0123456789ABCDEF';
+//       let hashtag = '#';
+//       for (let values = 0; values < 6; values++) {
+//         hashtag += hexValues[Math.floor(Math.random() * 16)];
+//         document.documentElement.style.setProperty('--color-primary', hashtag);
+//         console.log("random");
+//       }
+//     } else if (button.classList.contains('color-input')) {
+//       let input = document.getElementById('color-input');
+//       input.addEventListener('input', function inputColor() {
+//         document.documentElement.style.setProperty('--color-primary', input.value);
+//       })
+//     }
+//   }
+// }
+
 
 function hideIntroScreen() {
   let introScreen = document.getElementById('intro-screen');
