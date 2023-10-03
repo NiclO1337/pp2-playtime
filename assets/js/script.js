c6ec246ac57f43f0da6cff62c3ab295e2f212e25
NiclO1337 <niclas.hugdahl@gmail.com>
2023-10-03 14:01:56 +0200

    Clean up main document.addEventListener function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,41 +5,83 @@ document.addEventListener('DOMContentLoaded', function () {
   let buttons = document.getElementsByClassName("btn");
 
   for (let button of buttons) {
-    button.addEventListener('click', function changeColor() {
-      if (button.classList.contains('btn-blue')) {
-        document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)');
-      } else if (button.classList.contains('btn-red')) {
-        document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
-      } else if (button.classList.contains('btn-green')) {
-        document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
-      } else if (button.classList.contains('btn-random')) {
-        let hexValues = '0123456789ABCDEF';
-        let hashtag = '#';
-        for (let values = 0; values < 6; values++) {
-          hashtag += hexValues[Math.floor(Math.random() * 16)];
-        }
-        document.documentElement.style.setProperty('--color-primary', hashtag);
-      } else if (button.classList.contains('color-input')) {
-        let input = document.getElementById('color-input');
-        input.addEventListener('input', function inputColor() {
-          document.documentElement.style.setProperty('--color-primary', input.value);
-        })
-      }
-      if (button.classList.contains('play-game-btn')) {
+    button.addEventListener('click', function () {
+      if (button.classList.contains('theme-btn')) {
+        changeColor();
+      } else if (button.classList.contains('play-game-btn')) {
         let introScreen = document.getElementById('intro-screen');
-        introScreen.style.opacity = '0';        
+        introScreen.style.opacity = '0';
 
         window.setTimeout(hideIntroScreen, 900)
         window.setTimeout(showWelcomeText, 1100)
         window.setTimeout(hideWelcomeText, 2700)
         window.setTimeout(removeWelcomeText, 3600)
-        
+
         window.setTimeout(showGameScreen, 3700)
         window.setTimeout(visibleGameScreen, 3800)
       }
+      // if (button.classList.contains('btn-blue')) {
+      //   document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)');
+      // } else if (button.classList.contains('btn-red')) {
+      //   document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
+      // } else if (button.classList.contains('btn-green')) {
+      //   document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
+      // } else if (button.classList.contains('btn-random')) {
+      //   let hexValues = '0123456789ABCDEF';
+      //   let hashtag = '#';
+      //   for (let values = 0; values < 6; values++) {
+      //     hashtag += hexValues[Math.floor(Math.random() * 16)];
+      //     document.documentElement.style.setProperty('--color-primary', hashtag);
+      //   }        
+      // } else if (button.classList.contains('color-input')) {
+      //   let input = document.getElementById('color-input');
+      //   input.addEventListener('input', function inputColor() {
+      //     document.documentElement.style.setProperty('--color-primary', input.value);
+      //   })
+      // }
+      // if (button.classList.contains('play-game-btn')) {
+      //   let introScreen = document.getElementById('intro-screen');
+      //   introScreen.style.opacity = '0';        
+
+      //   window.setTimeout(hideIntroScreen, 900)
+      //   window.setTimeout(showWelcomeText, 1100)
+      //   window.setTimeout(hideWelcomeText, 2700)
+      //   window.setTimeout(removeWelcomeText, 3600)
+
+      //   window.setTimeout(showGameScreen, 3700)
+      //   window.setTimeout(visibleGameScreen, 3800)
+      // }
     })
   }
 })
+
+function changeColor() {
+  let buttons = document.getElementsByClassName("theme-btn");
+
+  for (let button of buttons) {
+    button.addEventListener('click', function () {
+      if (button.classList.contains('btn-blue')) {
+          document.documentElement.style.setProperty('--color-primary', 'rgb(6, 64, 119)');
+        } else if (button.classList.contains('btn-red')) {
+          document.documentElement.style.setProperty('--color-primary', 'rgb(180, 2, 2)');
+        } else if (button.classList.contains('btn-green')) {
+          document.documentElement.style.setProperty('--color-primary', 'rgb(64, 141, 29)');
+        } else if (button.classList.contains('btn-random')) {
+          let hexValues = '0123456789ABCDEF';
+          let hashtag = '#';
+          for (let values = 0; values < 6; values++) {
+            hashtag += hexValues[Math.floor(Math.random() * 16)];
+            document.documentElement.style.setProperty('--color-primary', hashtag);
+          }        
+        } else if (button.classList.contains('color-input')) {
+          let input = document.getElementById('color-input');
+          input.addEventListener('input', function inputColor() {
+            document.documentElement.style.setProperty('--color-primary', input.value);
+          })
+        }
+    })
+  }
+}
 
 function hideIntroScreen() {
   let introScreen = document.getElementById('intro-screen');
