df843ead193736b2ff6e26c9cb819f9f70bea280
NiclO1337 <niclas.hugdahl@gmail.com>
2023-10-05 17:34:30 +0200

    Added another validation for nickname input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,6 +123,11 @@ function playGameButton() {
       errorDiv.innerText = `Please enter a nickname, 
     I would like to know who I am
     about to demolish.`;
+    } else if (nickname.length > 15) {
+      errorDiv.innerText = `Please do not use more than 15 
+      characters. Just need a nickname, 
+      not your whole life story. 
+      Please and thank you.`
     } else {
       window.setTimeout(fadeIntroScreen, 0)
       window.setTimeout(hideIntroScreen, 900)
