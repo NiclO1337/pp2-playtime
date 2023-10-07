fdde6996f0a12be5b29131a58a738b9e4ed979a4
NiclO1337 <niclas.hugdahl@gmail.com>
2023-10-07 18:09:04 +0200

    Add more timers to gameOver function

--- assets/js/script.js
+++ assets/js/script.js
@@ -372,6 +372,11 @@ function gameOverPlayer() {
   window.setTimeout(colourWinner, 1000)
   window.setTimeout(showResultScreen, 1100)
   window.setTimeout(fadeInResultScreen, 1200)
+  window.setTimeout(showWinner, 1700)
+  window.setTimeout(fadeInWinner, 1800)
+  window.setTimeout(showSassyMessage, 1900)
+  window.setTimeout(fadeInSassyMessage, 2000)
+  
   
 }
 
